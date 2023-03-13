9c1c442174f0cdf48f570807d5d92831a399a478
daro-decagon <gadibiao@decagonhq.com>
2023-03-13 10:50:09 +0100

    changes default endpoint to "/"

--- src/main/java/org/example/HTTPServer/HttpServer.java
+++ src/main/java/org/example/HTTPServer/HttpServer.java
@@ -11,12 +11,12 @@ public class HttpServer {
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(8080);
 
-        while (true){
-            try(Socket socket = serverSocket.accept()) {
+        while (true) {
+            try (Socket socket = serverSocket.accept()) {
                 BufferedReader bufferedReader =
                         new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 String endpoint = bufferedReader.readLine().split(" ")[1];
-                if (endpoint.equals("/home")) {
+                if (endpoint.equals("/")) {
                     socket.getOutputStream()
                             .write(("HTTP/1.1 200 OK" + "\r\n\r\n" + "Welcome to HomePage")
                                     .getBytes("UTF-8"));
@@ -25,7 +25,9 @@ public class HttpServer {
                             .write(("HTTP/1.1 200 OK" + "\r\n\r\n" + "404 This page was not found.")
                                     .getBytes("UTF-8"));
                 }
-            }catch (IOException exception){
+            } catch (IOException exception) {
+            }
         }
     }
 }
+
